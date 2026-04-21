2c0e4a6746c18dfddbecab66a539dda8ac4573c5
Nonit <choudhary.nonit4@gmail.com>
2026-04-21 14:09:01 +0530

    Docker compose fix

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Day 2 working again fully 🚀");
+  res.send("Day 2 Retry for docker-compose 🚀");
 });
 
 app.listen(8080, () => {
